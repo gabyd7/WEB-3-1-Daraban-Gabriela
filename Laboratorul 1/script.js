8c4c3f71ce2ad8e6299542eac2d11666083d9d87
gabyd7 <daraban.gabriela00@gmail.com>
2026-09-09 22:48:56 +0300

    Adaugat dark light mode + mici schimbari

---
+++ Laboratorul 1/script.js
@@ -0,0 +1,18 @@
+const themeToggle = document.getElementById("themeToggle");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    themeToggle.textContent = "☀️";
+}
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeToggle.textContent = "☀️";
+        localStorage.setItem("theme", "dark");
+    } else {
+        themeToggle.textContent = "🌙";
+        localStorage.setItem("theme", "light");
+    }
+});
